d4e79b5c5542d2d1d11b829f3ad9bc3d42b8678b
Fabriziogianni7 <fabrizio@keyko.io>
2021-08-30 16:47:43 +0200

    added logs

--- src/index.ts
+++ src/index.ts
@@ -33,9 +33,9 @@ const api = new VerifyAPI( // eslint-disable-line
         process.env.NETWORK_TYPE !== "Mainnet" // if node != Mainnet => testnet = true
     ));
 
-const formatPK = () =>{
-    const BEGIN = config.beginPk 
-    const END = config.endPk 
+const formatPK = () => {
+    const BEGIN = config.beginPk
+    const END = config.endPk
     const splitted = config.privateKey.match(/.{1,64}/g);
     const formatted = `${BEGIN}\n${splitted.join("\n")}\n${END}`
     return formatted;
@@ -44,21 +44,21 @@ const formatPK = () =>{
 
 
 const octokit = new Octokit({
-        authStrategy: createAppAuth,
-        auth: {
-            type: "installation",
-            installationId: 16461975,
-            appId: config.appId,
-            privateKey: formatPK(),
-            clientId: config.clientId,
-            clientSecret: config.clientSecret
-        }
-    });
-    
+    authStrategy: createAppAuth,
+    auth: {
+        type: "installation",
+        installationId: 16461975,
+        appId: config.appId,
+        privateKey: formatPK(),
+        clientId: config.clientId,
+        clientSecret: config.clientSecret
+    }
+});
+
 
 const allocationDatacap = async () => {
     try {
-        console.info("Welcome to the auto-datacap-allocation-bot.")
+        console.log("Welcome to the auto-datacap-allocation-bot.")
         const rawIssues = await octokit.issues.listForRepo({
             owner,
             repo,
@@ -66,11 +66,13 @@ const allocationDatacap = async () => {
         })
 
         let issueInfoList: IssueInfo[] = []
+        console.log(`Number of fetched comments: ${rawIssues.data.length}`)
         for (const issue of rawIssues.data) {
             try {
-               
-                if (issue.labels.find((item: any) => item.name === "bot:readyToSign")) { 
-                    continue }
+                if (issue.labels.find((item: any) => item.name === "bot:readyToSign")) {
+                    console.log(`Issue number ${issue.number} skipped --> bot:readyToSign is present`)
+                    continue
+                }
 
                 //get all comments of a issue
                 const issueComments = await octokit.rest.issues.listComments({
@@ -79,31 +81,45 @@ const allocationDatacap = async () => {
                     issue_number: issue.number,
                 });
 
-                //Parse client address from issue description
-                const address = await parseIssue(issue.body).address
+                //Parse dc requested msig notary address and  client address
+                const { dataCapAllocated, msigAddress, clientAddress } = await findDatacapRequested(issueComments)
+                if (!dataCapAllocated && !clientAddress) {
+                    console.log(`Issue number ${issue.number} skipped --> DataCap allocation requested comment is not present`)
+                    continue
+                }
+                if (!clientAddress) {
+                    console.log(`Issue number ${issue.number} skipped --> clientAddressnot found after parsing the comments`)
+                    continue
+                }
+                if (!dataCapAllocated ) {
+                    console.log(`Issue number ${issue.number} skipped --> datacapAllocated not found after parsing the comments`)
+                    continue
+                }
 
-                //Parse dc requested notary address and 
-                const { dataCapAllocated, msigAddress } = await findDatacapRequested(issueComments)
-                // console.log("dataCapAllocated", dataCapAllocated, "msigAddress", msigAddress, "address", address)
-                if (!dataCapAllocated || !address) { continue }
 
-                //Check datacap remaining for this address
-                let actorAddress: any = await api.actorAddress(address) ? await api.actorAddress(address) : false
 
-                const checkClient = actorAddress ? await api.checkClient(actorAddress) : null
-                if (!checkClient) { continue }
+                //Check datacap remaining for this address 
+                // TODO check if dc on comment has really been allocated
+                let actorAddress: any = ""
+                if (clientAddress.startsWith("f1")) {
+                    actorAddress = await api.actorAddress(clientAddress) 
+                } else {
+                    actorAddress = await api.cachedActorAddress(clientAddress) 
+                }
+
+                const checkClient = await api.checkClient(actorAddress) 
 
                 const dataCapAllocatedConvert = dataCapAllocated.endsWith("B") ? anyToBytes(dataCapAllocated) : dataCapAllocated
                 const dataCapAllocatedBytes = Number(dataCapAllocatedConvert)
                 const dataCapRemainingBytes = Number(checkClient[0].datacap)
 
                 const margin = dataCapRemainingBytes / dataCapAllocatedBytes
-                console.log("margin", margin)
+                console.log("Margin", margin)
 
                 const info: IssueInfo = {
                     issueNumber: issue.number,
                     msigAddress,
-                    address: address,
+                    address: clientAddress,
                     actorAddress,
                     dcAllocationRequested: !dataCapAllocated.endsWith("B") ? bytesToiB(dataCapAllocated) : dataCapAllocated,
                     remainingDatacap: bytesToiB(dataCapRemainingBytes)
@@ -112,7 +128,7 @@ const allocationDatacap = async () => {
                 if (margin <= 0.75) {
                     const body = newAllocationRequestComment(info.address, info.dcAllocationRequested, info.remainingDatacap, info.msigAddress)
 
-                    console.info("CREATE REQUEST COMMENT")
+                    console.log("CREATE REQUEST COMMENT")
                     const commentResult = await octokit.issues.createComment({
                         owner,
                         repo,
@@ -131,16 +147,20 @@ const allocationDatacap = async () => {
                     issueInfoList.push(info)
                 }
 
-                console.log(info);
+                console.log("Info", info);
 
             } catch (error) {
+                console.log(`Error with the issue n ${issue.number}:`)
+                console.log(error)
+                console.log(`**Please, check that the datacap for the issue client has been granted**`)
                 continue
             }
         }
         commentStats(issueInfoList)
-        console.info(`auto-datacap-allocation-bot ended. number of issues commented: ${issueInfoList.length}`)
+        console.log(`Auto-datacap-allocation-bot ended. Number of issues commented: ${issueInfoList.length}`)
     } catch (error) {
-        console.warn(error)
+        console.error("error listing the issues, generic error in the bot")
+        console.error(error)
     }
 
 }
@@ -205,24 +225,27 @@ const commentStats = async (list: IssueInfo[]) => {
 
 }
 
-const findDatacapRequested = async (issueComments: any): Promise<{ dataCapAllocated: any; msigAddress: any; }> => {
+const findDatacapRequested = async (issueComments: any): Promise<{ dataCapAllocated: any; msigAddress: any; clientAddress: any }> => {
     try {
 
         let dc = ""
         let msigAddress = ""
+        let clientAddress = ""
 
         for (let i = issueComments.data.length - 1; i >= 0; i--) {
             const parseRequest = await parseReleaseRequest(issueComments.data[i].body)
             if (parseRequest.correct) {
                 dc = parseRequest.allocationDatacap
                 msigAddress = parseRequest.notaryAddress
+                clientAddress = parseRequest.clientAddress
                 break;
             }
         }
 
         return {
             dataCapAllocated: dc,
-            msigAddress
+            msigAddress,
+            clientAddress
         }
 
     } catch (error) {
