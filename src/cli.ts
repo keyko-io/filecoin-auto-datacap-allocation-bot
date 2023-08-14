221e28516a2430180b73339e85f5c09fd398d2a0
Charly López <charly@keyko.io>
2023-08-14 13:37:14 +0200

    Add cli to project
    now it can be launched via github actions

---
+++ src/cli.ts
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { msigTopup, exceptionMsigTopup } from "./msigTopup";
+import { clientsTopup_v2 } from "./clientTopup_v2";
+
+const program = new Command();
+
+program
+  .version('1.0.0')
+  .description('SSA Bot CLI');
+
+  program
+  .command('msig-topup')
+  .description('Check V3 multisig addresses DataCap')
+  .action(async () => {
+    try {
+      await msigTopup();
+    } catch (error) {
+      console.error('Error:', error.message);
+      process.exit(1);
+    }
+  });
+  
+  program
+    .command('exception-topup')
+    .description('Check exception V3 multisig addresses')
+    .action(async () => {
+      try {
+        await exceptionMsigTopup();
+      } catch (error) {
+        console.error('Error:', error.message);
+        process.exit(1);
+      }
+    });
+  
+  program
+    .command('clients-topup')
+    .description('Check issues and clients datacap')
+    .action(async () => {
+      try {
+        await clientsTopup_v2();
+      } catch (error) {
+        console.error('Error:', error.message);
+        process.exit(1);
+      }
+    });
+
+program.parse(process.argv);
