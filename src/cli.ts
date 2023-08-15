fa6d6bfae93e78bd2ad041c73f97a5810ff679db
Charly López <charly@keyko.io>
2023-08-15 11:23:26 +0200

    add check Issue

--- src/cli.ts
+++ src/cli.ts
@@ -53,6 +53,28 @@ program
     .action(async () => {
       console.log(`If everything is ok, you should see the following health check issue: ${config.healthCheckIssue * 2}`)
     });
+  
+  program
+    .command('check-issue')  // Nombre del nuevo comando
+    .description('Check issue details')
+    .option('-n, --issueNumber <number>', 'Issue Number')
+    .option('-i, --issueBody <string>', 'Issue Body')
+    .option('-c, --commentBody <string>', 'Comment Body')
+    .option('-a, --commentAuthor <string>', 'Comment Author')
+    .action(async (cmdObj) => {
+        try {
+            await checkIssue(Number(cmdObj.issueNumber), cmdObj.issueBody, cmdObj.commentBody, cmdObj.commentAuthor);
+        } catch (error) {
+            console.error('Error:', error.message);
+            process.exit(1);
+        }
+    });
 
+  function checkIssue(issueNumber: Number, issueBody: String, commentBody: String, commentAuthor: String) {
+    console.log(`Issue Number: ${issueNumber}`);
+    console.log(`Issue Body: ${issueBody}`);
+    console.log(`Comment Body: ${commentBody}`);
+    console.log(`Comment Author: ${commentAuthor}`);
+  }
 
 program.parse(process.argv);
